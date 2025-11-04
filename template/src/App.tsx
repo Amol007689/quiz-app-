a9062e4476b1482684c4c92aaf792258274d433f
Riya Joseph <riyajoseph2212@gmail.com>
2025-11-04 23:09:14 +0530

    Riya

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/SignUp";
+import Wallet from "./pages/Wallet";
 // import Dashboard from "./pages/dashboard";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/wallet" element={<Wallet />} />
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </Router>
